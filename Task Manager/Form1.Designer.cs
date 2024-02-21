a742b528a8598f643b65bd3437e1cf14a04b7af4
eewpo <eewpo@DESKTOP-6Q0QDSG>
2024-02-21 17:46:11 +0700

    Добавьте файлы проекта.

---
+++ Task Manager/Form1.Designer.cs
@@ -0,0 +1,254 @@
+﻿namespace Task_Manager
+{
+    partial class Form1
+    {
+        /// <summary>
+        /// Обязательная переменная конструктора.
+        /// </summary>
+        private System.ComponentModel.IContainer components = null;
+
+        /// <summary>
+        /// Освободить все используемые ресурсы.
+        /// </summary>
+        /// <param name="disposing">истинно, если управляемый ресурс должен быть удален; иначе ложно.</param>
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing && (components != null))
+            {
+                components.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+
+        #region Код, автоматически созданный конструктором форм Windows
+
+        /// <summary>
+        /// Требуемый метод для поддержки конструктора — не изменяйте 
+        /// содержимое этого метода с помощью редактора кода.
+        /// </summary>
+        private void InitializeComponent()
+        {
+            this.components = new System.ComponentModel.Container();
+            System.ComponentModel.ComponentResourceManager resources = new System.ComponentModel.ComponentResourceManager(typeof(Form1));
+            this.contextMenuStrip1 = new System.Windows.Forms.ContextMenuStrip(this.components);
+            this.fileToolStripMenuItem = new System.Windows.Forms.ToolStripMenuItem();
+            this.startATaskToolStripMenuItem = new System.Windows.Forms.ToolStripMenuItem();
+            this.exitToolStripMenuItem = new System.Windows.Forms.ToolStripMenuItem();
+            this.toolStrip1 = new System.Windows.Forms.ToolStrip();
+            this.toolStripButton1 = new System.Windows.Forms.ToolStripButton();
+            this.toolStripButton2 = new System.Windows.Forms.ToolStripButton();
+            this.toolStripButton3 = new System.Windows.Forms.ToolStripButton();
+            this.toolStrip2 = new System.Windows.Forms.ToolStrip();
+            this.toolStripLabel1 = new System.Windows.Forms.ToolStripLabel();
+            this.toolStripTextBox1 = new System.Windows.Forms.ToolStripTextBox();
+            this.listView1 = new System.Windows.Forms.ListView();
+            this.columnHeader1 = ((System.Windows.Forms.ColumnHeader)(new System.Windows.Forms.ColumnHeader()));
+            this.columnHeader2 = ((System.Windows.Forms.ColumnHeader)(new System.Windows.Forms.ColumnHeader()));
+            this.contextMenuStrip2 = new System.Windows.Forms.ContextMenuStrip(this.components);
+            this.completeToolStripMenuItem = new System.Windows.Forms.ToolStripMenuItem();
+            this.completeTheProcessTreeToolStripMenuItem = new System.Windows.Forms.ToolStripMenuItem();
+            this.contextMenuStrip1.SuspendLayout();
+            this.toolStrip1.SuspendLayout();
+            this.toolStrip2.SuspendLayout();
+            this.contextMenuStrip2.SuspendLayout();
+            this.SuspendLayout();
+            // 
+            // contextMenuStrip1
+            // 
+            this.contextMenuStrip1.Font = new System.Drawing.Font("Segoe UI", 18F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(204)));
+            this.contextMenuStrip1.Items.AddRange(new System.Windows.Forms.ToolStripItem[] {
+            this.fileToolStripMenuItem,
+            this.startATaskToolStripMenuItem,
+            this.exitToolStripMenuItem});
+            this.contextMenuStrip1.Name = "contextMenuStrip1";
+            this.contextMenuStrip1.Size = new System.Drawing.Size(205, 134);
+            // 
+            // fileToolStripMenuItem
+            // 
+            this.fileToolStripMenuItem.Name = "fileToolStripMenuItem";
+            this.fileToolStripMenuItem.Size = new System.Drawing.Size(204, 36);
+            this.fileToolStripMenuItem.Text = "File";
+            // 
+            // startATaskToolStripMenuItem
+            // 
+            this.startATaskToolStripMenuItem.Name = "startATaskToolStripMenuItem";
+            this.startATaskToolStripMenuItem.Size = new System.Drawing.Size(204, 36);
+            this.startATaskToolStripMenuItem.Text = "Start a task";
+            this.startATaskToolStripMenuItem.Click += new System.EventHandler(this.startATaskToolStripMenuItem_Click);
+            // 
+            // exitToolStripMenuItem
+            // 
+            this.exitToolStripMenuItem.Name = "exitToolStripMenuItem";
+            this.exitToolStripMenuItem.Size = new System.Drawing.Size(204, 36);
+            this.exitToolStripMenuItem.Text = "Exit";
+            this.exitToolStripMenuItem.Click += new System.EventHandler(this.exitToolStripMenuItem_Click);
+            // 
+            // toolStrip1
+            // 
+            this.toolStrip1.Font = new System.Drawing.Font("Segoe UI", 18F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(204)));
+            this.toolStrip1.Items.AddRange(new System.Windows.Forms.ToolStripItem[] {
+            this.toolStripButton1,
+            this.toolStripButton2,
+            this.toolStripButton3});
+            this.toolStrip1.Location = new System.Drawing.Point(0, 0);
+            this.toolStrip1.Name = "toolStrip1";
+            this.toolStrip1.Size = new System.Drawing.Size(799, 28);
+            this.toolStrip1.TabIndex = 1;
+            this.toolStrip1.Text = "toolStrip1";
+            // 
+            // toolStripButton1
+            // 
+            this.toolStripButton1.DisplayStyle = System.Windows.Forms.ToolStripItemDisplayStyle.Text;
+            this.toolStripButton1.Font = new System.Drawing.Font("Segoe UI", 12F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(204)));
+            this.toolStripButton1.Image = ((System.Drawing.Image)(resources.GetObject("toolStripButton1.Image")));
+            this.toolStripButton1.ImageTransparentColor = System.Drawing.Color.Magenta;
+            this.toolStripButton1.Name = "toolStripButton1";
+            this.toolStripButton1.Size = new System.Drawing.Size(64, 25);
+            this.toolStripButton1.Text = "Update";
+            this.toolStripButton1.Click += new System.EventHandler(this.toolStripButton1_Click);
+            // 
+            // toolStripButton2
+            // 
+            this.toolStripButton2.DisplayStyle = System.Windows.Forms.ToolStripItemDisplayStyle.Text;
+            this.toolStripButton2.Font = new System.Drawing.Font("Segoe UI", 12F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(204)));
+            this.toolStripButton2.Image = ((System.Drawing.Image)(resources.GetObject("toolStripButton2.Image")));
+            this.toolStripButton2.ImageTransparentColor = System.Drawing.Color.Magenta;
+            this.toolStripButton2.Name = "toolStripButton2";
+            this.toolStripButton2.Size = new System.Drawing.Size(81, 25);
+            this.toolStripButton2.Text = "Complete";
+            this.toolStripButton2.Click += new System.EventHandler(this.toolStripButton2_Click);
+            // 
+            // toolStripButton3
+            // 
+            this.toolStripButton3.DisplayStyle = System.Windows.Forms.ToolStripItemDisplayStyle.Text;
+            this.toolStripButton3.Font = new System.Drawing.Font("Segoe UI", 12F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(204)));
+            this.toolStripButton3.Image = ((System.Drawing.Image)(resources.GetObject("toolStripButton3.Image")));
+            this.toolStripButton3.ImageTransparentColor = System.Drawing.Color.Magenta;
+            this.toolStripButton3.Name = "toolStripButton3";
+            this.toolStripButton3.Size = new System.Drawing.Size(195, 25);
+            this.toolStripButton3.Text = "Complete the process tree";
+            this.toolStripButton3.Click += new System.EventHandler(this.toolStripButton3_Click);
+            // 
+            // toolStrip2
+            // 
+            this.toolStrip2.Font = new System.Drawing.Font("Segoe UI", 18F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(204)));
+            this.toolStrip2.Items.AddRange(new System.Windows.Forms.ToolStripItem[] {
+            this.toolStripLabel1,
+            this.toolStripTextBox1});
+            this.toolStrip2.Location = new System.Drawing.Point(0, 28);
+            this.toolStrip2.Name = "toolStrip2";
+            this.toolStrip2.Size = new System.Drawing.Size(799, 25);
+            this.toolStrip2.TabIndex = 2;
+            this.toolStrip2.Text = "toolStrip2";
+            // 
+            // toolStripLabel1
+            // 
+            this.toolStripLabel1.Font = new System.Drawing.Font("Segoe UI", 12F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(204)));
+            this.toolStripLabel1.Name = "toolStripLabel1";
+            this.toolStripLabel1.Size = new System.Drawing.Size(45, 22);
+            this.toolStripLabel1.Text = "Filter";
+            // 
+            // toolStripTextBox1
+            // 
+            this.toolStripTextBox1.Font = new System.Drawing.Font("Segoe UI", 9F);
+            this.toolStripTextBox1.Name = "toolStripTextBox1";
+            this.toolStripTextBox1.Size = new System.Drawing.Size(100, 25);
+            this.toolStripTextBox1.TextChanged += new System.EventHandler(this.toolStripTextBox1_TextChanged);
+            // 
+            // listView1
+            // 
+            this.listView1.Columns.AddRange(new System.Windows.Forms.ColumnHeader[] {
+            this.columnHeader1,
+            this.columnHeader2});
+            this.listView1.ContextMenuStrip = this.contextMenuStrip1;
+            this.listView1.Dock = System.Windows.Forms.DockStyle.Fill;
+            this.listView1.FullRowSelect = true;
+            this.listView1.GridLines = true;
+            this.listView1.HideSelection = false;
+            this.listView1.Location = new System.Drawing.Point(0, 53);
+            this.listView1.MultiSelect = false;
+            this.listView1.Name = "listView1";
+            this.listView1.Size = new System.Drawing.Size(799, 239);
+            this.listView1.TabIndex = 3;
+            this.listView1.UseCompatibleStateImageBehavior = false;
+            this.listView1.View = System.Windows.Forms.View.Details;
+            this.listView1.ColumnClick += new System.Windows.Forms.ColumnClickEventHandler(this.listView1_ColumnClick);
+            this.listView1.SelectedIndexChanged += new System.EventHandler(this.listView1_SelectedIndexChanged);
+            // 
+            // columnHeader1
+            // 
+            this.columnHeader1.Text = "Name of the process";
+            this.columnHeader1.Width = 300;
+            // 
+            // columnHeader2
+            // 
+            this.columnHeader2.Text = "Memory used";
+            this.columnHeader2.Width = 300;
+            // 
+            // contextMenuStrip2
+            // 
+            this.contextMenuStrip2.Items.AddRange(new System.Windows.Forms.ToolStripItem[] {
+            this.completeToolStripMenuItem,
+            this.completeTheProcessTreeToolStripMenuItem});
+            this.contextMenuStrip2.Name = "contextMenuStrip2";
+            this.contextMenuStrip2.Size = new System.Drawing.Size(213, 48);
+            // 
+            // completeToolStripMenuItem
+            // 
+            this.completeToolStripMenuItem.Name = "completeToolStripMenuItem";
+            this.completeToolStripMenuItem.Size = new System.Drawing.Size(212, 22);
+            this.completeToolStripMenuItem.Text = "Complete";
+            // 
+            // completeTheProcessTreeToolStripMenuItem
+            // 
+            this.completeTheProcessTreeToolStripMenuItem.Name = "completeTheProcessTreeToolStripMenuItem";
+            this.completeTheProcessTreeToolStripMenuItem.Size = new System.Drawing.Size(212, 22);
+            this.completeTheProcessTreeToolStripMenuItem.Text = "Сomplete the process tree";
+            this.completeTheProcessTreeToolStripMenuItem.Click += new System.EventHandler(this.completeTheProcessTreeToolStripMenuItem_Click);
+            // 
+            // Form1
+            // 
+            this.AutoScaleDimensions = new System.Drawing.SizeF(13F, 27F);
+            this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
+            this.ClientSize = new System.Drawing.Size(799, 292);
+            this.Controls.Add(this.listView1);
+            this.Controls.Add(this.toolStrip2);
+            this.Controls.Add(this.toolStrip1);
+            this.Font = new System.Drawing.Font("Times New Roman", 18F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(204)));
+            this.Margin = new System.Windows.Forms.Padding(6);
+            this.Name = "Form1";
+            this.Text = "Task Manager";
+            this.Load += new System.EventHandler(this.Form1_Load);
+            this.contextMenuStrip1.ResumeLayout(false);
+            this.toolStrip1.ResumeLayout(false);
+            this.toolStrip1.PerformLayout();
+            this.toolStrip2.ResumeLayout(false);
+            this.toolStrip2.PerformLayout();
+            this.contextMenuStrip2.ResumeLayout(false);
+            this.ResumeLayout(false);
+            this.PerformLayout();
+
+        }
+
+        #endregion
+
+        private System.Windows.Forms.ContextMenuStrip contextMenuStrip1;
+        private System.Windows.Forms.ToolStripMenuItem fileToolStripMenuItem;
+        private System.Windows.Forms.ToolStripMenuItem exitToolStripMenuItem;
+        private System.Windows.Forms.ToolStrip toolStrip1;
+        private System.Windows.Forms.ToolStripButton toolStripButton1;
+        private System.Windows.Forms.ToolStripButton toolStripButton2;
+        private System.Windows.Forms.ToolStripButton toolStripButton3;
+        private System.Windows.Forms.ToolStrip toolStrip2;
+        private System.Windows.Forms.ToolStripLabel toolStripLabel1;
+        private System.Windows.Forms.ToolStripTextBox toolStripTextBox1;
+        private System.Windows.Forms.ListView listView1;
+        private System.Windows.Forms.ColumnHeader columnHeader1;
+        private System.Windows.Forms.ColumnHeader columnHeader2;
+        private System.Windows.Forms.ContextMenuStrip contextMenuStrip2;
+        private System.Windows.Forms.ToolStripMenuItem completeToolStripMenuItem;
+        private System.Windows.Forms.ToolStripMenuItem completeTheProcessTreeToolStripMenuItem;
+        private System.Windows.Forms.ToolStripMenuItem startATaskToolStripMenuItem;
+    }
+}
+
---
+++ Task Manager/Form1.cs
@@ -0,0 +1,260 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Drawing;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+using System.Diagnostics;
+using System.Management;
+using Microsoft.VisualBasic;
+
+namespace Task_Manager
+{
+    public partial class Form1 : Form
+    {
+        private List<Process> processes = null;// главный список в котором храняться все процессы 
+        private ListViewitemComparer Comparer = null;
+        
+        public Form1()
+        {
+            InitializeComponent();
+        }
+
+        private void listView1_SelectedIndexChanged(object sender, EventArgs e)
+        {
+
+        }
+        // метод который работает только со списком бэкэндом 
+        private void GetProcesses() // метод создает список и обновляет его 
+        {
+            processes.Clear();// очистка списка 
+            processes = Process.GetProcesses().ToList<Process>();// заполнение списка заново получаем все системные процессы 
+
+        }
+        // метод который работает с листвью фронтендом 
+        private void RefreshProcessesList()
+        {
+            try
+            {
+                listView1.Items.Clear();// очистим лист 
+                double memSize = 0;// переменная при переборе всех процессов храниться память 
+                foreach (Process p in processes) // с помощью цикла переберем все процессы 
+                {
+                    if (p != null)
+                    {
+                        memSize = 0;
+                        PerformanceCounter pc = new PerformanceCounter();
+                        pc.CategoryName = "Process";
+                        pc.CounterName = "Working Set - Private";
+                        pc.InstanceName = p.ProcessName;
+                        memSize = (double)pc.NextValue() / (1000 * 1000);
+                        string[] row = new string[] { p.ProcessName.ToString(), Math.Round(memSize, 1).ToString() };
+                        listView1.Items.Add(new ListViewItem(row));
+                        pc.Close();
+                        pc.Dispose();
+                    }
+
+
+                }
+                Text = "running processes:" + processes.Count.ToString();// текст для шапки формы 
+                                                                         // Перегрузка метода фильтровать процессы по имени 
+
+
+            }
+            catch (Exception) { }
+        }
+        private void RefreshProcessesList(List<Process> processes, string keyword)
+        {
+            listView1.Items.Clear();// очистим лист 
+            double memSize = 0;// переменная при переборе всех процессов храниться память 
+            foreach (Process p in processes) // с помощью цикла переберем все процессы 
+            {
+                memSize = 0;
+                PerformanceCounter pc = new PerformanceCounter();
+                pc.CategoryName = "Process";
+                pc.CounterName = "Working Set - Private";
+                pc.InstanceName = p.ProcessName;
+                memSize = (double)pc.NextValue() / (1000 * 1000);
+                string[] row = new string[] { p.ProcessName.ToString(), Math.Round(memSize, 1).ToString() };
+                listView1.Items.Add(new ListViewItem(row));
+                pc.Close();
+                pc.Dispose();
+
+            }
+            Text = $"running processes '{keyword}':" + processes.Count.ToString();// текст для шапки формы 
+
+
+        }
+        // метод для завершения одного процесса который мы выбрали 
+        private void KillProcess(Process process)
+        {
+            process.Kill();
+            process.WaitForExit();
+        }
+        // метод завершения дерева процессов 
+        private void KillProcessAndChildrem(int pid)
+        {
+            if (pid == 0)
+            {
+                return; // проверка что пид не равен 0
+            }
+            // находит все связанные процессы по айди
+            ManagementObjectSearcher searcher = new ManagementObjectSearcher(
+                "Select * From Win32_Process Where ParentProcessID=" + pid);
+            ManagementObjectCollection objectCollection = searcher.Get();
+            foreach (ManagementObject obj in objectCollection)
+            {
+                KillProcessAndChildrem(Convert.ToInt32(obj["ProcessID"]));
+            }
+            try
+            {
+                Process p = Process.GetProcessById(pid);
+                p.Kill();
+                p.WaitForExit();
+            }
+            catch (ArgumentException)
+            {
+
+                
+            }
+        }
+        // метод для получения айди родительского процесса
+        private int GetParentProcessId(Process p)
+        {
+            int parentID = 0;
+            try
+            {
+                ManagementObject managementObject = new ManagementObject("win32_process.handle='"+ p.Id + "'");
+                managementObject.Get();
+                parentID = Convert.ToInt32(managementObject["ParentProcessId"]);
+
+
+
+            }
+            catch (Exception)
+            {
+                
+            }
+            return parentID;
+
+        }
+
+        private void Form1_Load(object sender, EventArgs e)// обработчик события в шапке 
+        {
+            processes = new List<Process>();// инициализация списка 
+            GetProcesses();
+            RefreshProcessesList();// заполняет собятия лист вью
+            Comparer = new ListViewitemComparer();
+            Comparer.ColumnIndex = 0;
+
+        }
+
+        private void toolStripButton1_Click(object sender, EventArgs e)// обработчик кнопки обновить 
+        {
+            GetProcesses ();// вызываем 2 метода 
+            RefreshProcessesList ();
+        }
+
+        private void toolStripButton2_Click(object sender, EventArgs e)// обработчик кнопки завершить 
+        {
+            try
+            {
+                if (listView1.SelectedItems[0] != null)
+                {
+                    Process processToKill = processes.Where((x) => x.ProcessName ==
+                    listView1.SelectedItems[0].SubItems[0].Text).ToList()[0];
+                    KillProcess (processToKill);
+                    GetProcesses();
+                    RefreshProcessesList();
+
+                }
+            }
+            catch (Exception)
+            {
+
+                
+            }
+        }
+
+        private void toolStripButton3_Click(object sender, EventArgs e)// обработчик дерева процессов 
+        {
+            try
+            {
+                if (listView1.SelectedItems[0] != null)
+                {
+                    Process processToKill = processes.Where((x) => x.ProcessName ==
+                    listView1.SelectedItems[0].SubItems[0].Text).ToList()[0];
+                    KillProcessAndChildrem(GetParentProcessId(processToKill));
+                    GetProcesses();
+                    RefreshProcessesList();
+                }
+            }
+            catch (Exception)
+            {
+
+
+            }
+        }
+
+        private void completeTheProcessTreeToolStripMenuItem_Click(object sender, EventArgs e)// обработчик дерева процессов 
+        {
+            try
+            {
+                if (listView1.SelectedItems[0] != null)
+                {
+                    Process processToKill = processes.Where((x) => x.ProcessName ==
+                    listView1.SelectedItems[0].SubItems[0].Text).ToList()[0];
+                    KillProcessAndChildrem(GetParentProcessId(processToKill));
+                    GetProcesses();
+                    RefreshProcessesList();
+                }
+            }
+            catch (Exception)
+            {
+
+
+            }
+        }
+
+        private void startATaskToolStripMenuItem_Click(object sender, EventArgs e)// обработчик запустить задачу 
+        {
+            string path = Interaction.InputBox("enter the program name", "starting a new task");
+            try
+            {
+                Process.Start(path);
+            }
+            catch (Exception)
+            {
+
+                throw;
+            }
+        }
+
+        private void toolStripTextBox1_TextChanged(object sender, EventArgs e)
+        {
+            GetProcesses();
+            List<Process> filteredprocesses = processes.Where((x) =>
+            x.ProcessName.ToLower().Contains(toolStripTextBox1.Text.ToLower())).ToList<Process>();
+            RefreshProcessesList(filteredprocesses, toolStripTextBox1.Text);
+
+
+        }
+
+        private void listView1_ColumnClick(object sender, ColumnClickEventArgs e)
+        {
+            Comparer.ColumnIndex = e.Column;
+            Comparer.SortDirection = Comparer.SortDirection == SortOrder.Ascending 
+                ? SortOrder.Descending : SortOrder.Ascending;
+            listView1.ListViewItemSorter = Comparer;
+            listView1.Sort();
+        }
+
+        private void exitToolStripMenuItem_Click(object sender, EventArgs e)
+        {
+            Application.Exit();
+        }
+    }
+}
